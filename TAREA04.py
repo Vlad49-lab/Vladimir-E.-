1604e061a36979d23ec1d64a0312c29959e87910
Vlad49-lab <vladimirespinosa49@gmail.com>
2024-08-28 12:20:00 -0600

    Tarea 04

---
+++ TAREA04.py
@@ -0,0 +1,57 @@
+# 01 PROMEDIO DEL TOTAL
+calif_1 = 10
+calif_2 = 7
+calif_3 = 4
+calificacion = (.15 * calif_1) + (.35 * calif_2) + (.5 * calif_3)
+
+print(calificacion)
+
+# Matriz y cuarto valor
+
+matriz = [ [1, 1, 1, 3], [2, 2, 2, 7], [3, 3, 3, 9], [4, 4, 4, 13] ]
+
+
+primer_elemento = [matriz[0][0],matriz[1][0],matriz[2][0],matriz[3][0]]
+segundo_elemento =[matriz[0][1],matriz[1][1],matriz[2][1],matriz[3][1]]
+tercer_elemento =[matriz[0][2],matriz[1][2],matriz[2][2],matriz[3][2]]
+cuarto_elemento = [matriz[0][3],matriz[1][3],matriz[2][3],matriz[3][3]]
+
+for x in cuarto_elemento :
+
+    if x == matriz[0][3]:
+        if x != matriz[0][0] + matriz[0][1] + matriz [0][2]:
+            matriz[0][3] = matriz[0][0] + matriz[0][1] + matriz [0][2]
+
+            continue
+        else:
+            continue
+
+    elif x == matriz[1][3]:
+        if x != matriz[1][0] + matriz[1][1] + matriz [1][2]:
+            matriz[1][3] = matriz[1][0] + matriz[1][1] + matriz [1][2]
+            continue
+
+        else:
+            continue
+
+    elif x == matriz[2][3]:
+        if x != matriz[2][0] + matriz[2][1] + matriz [2][2]:
+            matriz[2][3] = matriz[2][0] + matriz[2][1] + matriz [2][2]
+            continue
+
+        else:
+            continue
+
+    elif x == matriz[3][3]:
+        if x != matriz[3][0] + matriz[3][1] + matriz [3][2]:
+            matriz[3][3] = matriz[3][0] + matriz[3][1] + matriz [3][2]
+            continue
+
+        else:
+            continue
+
+    else:
+        print('no es posible')
+        continue
+
+print(matriz)
